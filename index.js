e111621080ba079f3cc73fdaf8f35b4ab722309d
ErnestH1 <hansonernest7@gmail.com>
2022-11-22 21:40:43 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,35 @@
+
+function add(a, b){
+    let sum = a + b;
+    return sum
+}
+function subtract(a, b){
+    let sub = a -b;
+    return sub
+}
+function multiply(a, b){
+    let mult = a * b;
+    return mult
+}
+function divide(a, b){
+    let dev = a / b;
+    return dev
+}
+function increment(n){
+    let inc = n;
+    inc++
+    return inc
+}
+function decrement(n){
+    let dec = n;
+    dec--
+    return dec
+}
+function makeInt(n){
+   let make = parseInt(n, 10)
+    
+    return make
+}
+function preserveDecimal(n){
+    return (parseFloat(n))
+ }
